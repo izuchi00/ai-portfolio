45a3eed5eb47c97aec9501e92d8cf158e0884902
[dyad] <git@dyad.sh>
2025-10-06 06:01:26 +0100

    [dyad] Explicitly imported React in App.tsx and main.tsx, and requested a full rebuild - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import React from "react";
+import * as React from "react"; // Explicitly import React
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-// Temporarily removing ThemeProvider to simplify and isolate the error
-// import { ThemeProvider } from "./components/layout/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <ThemeProvider defaultTheme="system" attribute="class"> */}
-      <App />
-    {/* </ThemeProvider> */}
+    <App />
   </React.StrictMode>
 );
